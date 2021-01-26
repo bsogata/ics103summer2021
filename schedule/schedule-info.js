085df63e0b48496b4f29aa3b08713f1e8876a30b
Branden Ogata <branden.s.ogata@gmail.com>
2021-01-26 11:55:10 -1000

    Initial publish.

---
+++ schedule/schedule-info.js
@@ -0,0 +1,4 @@
+moreaEventData = [
+  {title: "Learn to Baz", url: "/ics103summer2021/modules/baz", start: "2015-06-11", end: "2015-06-15"},
+  {title: "Experience the Baz", url: "/ics103summer2021/morea/baz/experience.html", start: "2015-06-11T18:00"}
+]
